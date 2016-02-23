faea6d6a5b4c6b5f61ed41ee70ba049b9fd235a8
Abe <zach.abe@gmail.com>
2016-02-23 11:47:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 
     
-
-    
     var text = document.getElementsByClassName("quantity");
-   
-    
-var portionsInHeader =["en","två","tre","fyra","fem"];
+    var portionsInHeader =["en person","två personer","tre personer","fyra personer","fem personer"];
     var prevNr=1;
+    
     function changeNumber(number, pos){
         for(var i=0; i<14; i++){
        
-        var textSize = parseInt(text[i].innerHTML);
+        var textSize = parseFloat(text[i].innerHTML);
         var total = number*textSize/prevNr;
         text[i].innerHTML=total;
             
@@ -21,9 +18,8 @@ var portionsInHeader =["en","två","tre","fyra","fem"];
     }
 
     if(typeof(Storage) !="undefined"){
-        changeNumber(localStorage.getItem("totPortion"),1);
+        changeNumber(localStorage.getItem("totPortion"),localStorage.getItem("totPortion")-1);
         document.getElementsByClassName("quantity").innerHTML=localStorage.getItem("totPortion");
-        //alert(localStorage.getItem("totPortion"));
                               
     }
 
@@ -35,20 +31,20 @@ else{
     $(document).ready(function(){
 
    
-    $(document).ajaxStart(function(){
-        jQuery("#loading").fadeIn();
-//jQuery("#preloader").delay(1000).fadeIn("slow");
-        console.log("LADDAR IN");
-    });
-        
-        $(document).ajaxStop(function(){
-        jQuery("#loading").fadeOut(2000);
-//jQuery("#preloader").delay(1000).fadeOut("slow");
-            console.log("LADDADE KLART");
-    });
         
     jQuery(window).load(function(){
-        $("div").load("ajaxTest.asp");
+         $.ajax({
+                method: "GET",
+                url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=f7cac50bfbcb0891&recipe=apfelstrudel",
+                success: function(data){
+
+                     $('#averageRating').text(data.rating);
+                     $('#nrCount').text(data.votes);
+                     document.getElementById("rating").innerHTML=localStorage.getItem("yourRating");
+                     jQuery("#loading").fadeOut(2000);
+            }
+            })
+       
     });
 
         
@@ -57,6 +53,10 @@ else{
          $(".star").hover(function(){
         $("img").fadeOut(1000);
              $("img").fadeIn(1000);
+    });
+        
+         $(".star").mouseleave(function(){
+                $(this).prevUntil(".lederhosen").andSelf().attr("src", "Green.png");
     });
 
    $(".star").hover(function(){
@@ -71,7 +71,7 @@ else{
         var prevStar=0;
         $(".star").click(function(){
             $(this).prevUntil(".lederhosen").andSelf().attr("src", "PurpTaken.png");
-     //    $(".star").unbind().prevAll().andSelf();
+     
             var idClick = $(this).attr('id');
             var clickValue = parseInt(idClick);
             
@@ -79,24 +79,31 @@ else{
             var ratingText = document.getElementById("rating");
             ratingText.innerHTML=idClick;
             count++;
+            localStorage.setItem("nrOfVotes",count);
             
             average=(clickValue+prevStar)/count;
+           
             
             prevStar=clickValue+prevStar;
-            var averageRatingText = document.getElementById("averageRating");
-            averageRatingText.innerHTML=average;
-            document.getElementById("nrCount").innerHTML=count;
-            
-           
-          //  $(this).unbind('hover').prevUntil(".lederhosen").andSelf.nextAll().attr('disabled', 'disabled');
-            
-            
+      
     });
+        //API-NYCKEL: f7cac50bfbcb0891
+        $('.star').click(function(){
+            var theId = $(this).attr('id');
+            var idParse = parseInt(theId);
+            $.ajax({
+                method: "GET",
+                url: "https://edu.oscarb.se/sjk15/api/recipe/?api_key=f7cac50bfbcb0891&recipe=apfelstrudel&rating="+idParse,
+                success: function(){
+                alert("hej du röstade" + idParse);
+                localStorage.setItem("yourRating", idParse);
+            }
+            })
+        });
         
         $("Strong").one("click",function(){
             $(this).css("text-decoration", "line-through");
-          //  alert("You rated it"+ .star)
-          //  $(this).unbind('hover').prevUntil(".lederhosen").andSelf.nextAll().attr('disabled', 'disabled');
+          
             
     });
         
